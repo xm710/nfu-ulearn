b2aed7f1a24d08599b565e2a2cbdf49eee0aceb3
xm710 <a0983382322@gmail.com>
2026-06-20 00:03:19 +0800

    刪除main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from ulearn import ULEARN
-import pickle
-
-ULEARN(username="41425243", password="")
-
-with open("./cookies/41425243_20260619.pkl", "rb") as f:
-    cookies = pickle.load(f)
-    ULEARN(cookies=cookies)
