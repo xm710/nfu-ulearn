8d8692f99366ec0418f8fc83b037311c5f2f55f4
xm710 <a0983382322@gmail.com>
2026-06-20 12:59:14 +0800

    更新憑證名稱

--- usage.py
+++ usage.py
@@ -6,6 +6,6 @@ ULEARN(username="學號", password="密碼")
 
 
 # 2. 憑證登入
-with open("./cookies/41425243_20260619.pkl", "rb") as f:
+with open("./cookies/*.pkl", "rb") as f:
     cookies = pickle.load(f)
     ULEARN(cookies=cookies)
